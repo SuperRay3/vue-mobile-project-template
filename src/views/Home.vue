58c7ba5c895a09750b16d808bd262a2edca04fe5
leijialiang <ljl519525771@qq.com>
2022-02-22 16:23:01 +0800

    feat: 增加 composition-api

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <h1>vue-mobile-project-template</h1>
+    <section>
+      <h3>现有功能</h3>
+      <ul>
+        <li></li>
+      </ul>
+    </section>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  name: 'HomeView',
+  components: {},
+  setup() {
+    return {}
+  }
+})
+</script>
