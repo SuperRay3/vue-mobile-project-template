e219d1bf354402a5b7e4f85c71bb6417e00d3963
leijialiang <ljl519525771@qq.com>
2022-02-22 16:35:44 +0800

    增加 remove console

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
+const prodPlugins = []
+
+if (process.env.NODE_ENV === 'production') {
+  prodPlugins.push([
+    'transform-remove-console',
+    {
+      exclude: ['error', 'warn']
+    }
+  ])
+}
+
 module.exports = {
-  presets: ['@vue/cli-plugin-babel/preset']
+  presets: ['@vue/cli-plugin-babel/preset'],
+  plugins: [...prodPlugins]
 }
